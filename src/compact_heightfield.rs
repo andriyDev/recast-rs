7b5e00133176eb212df7a5d062f489778a4c40a6
andriyDev <andriydzikh@gmail.com>
2023-04-23 11:43:45 -0700

    Add accessors for spans in a CompactHeightfield.
    This also gives us better testing! :D

--- src/compact_heightfield.rs
+++ src/compact_heightfield.rs
@@ -1,8 +1,9 @@
-use std::ops::{Deref, DerefMut};
+use std::ops::{Deref, DerefMut, Range};
 
 use recastnavigation_sys::{
   rcBuildCompactHeightfield, rcBuildDistanceField, rcBuildLayerRegions,
-  rcBuildRegions, rcBuildRegionsMonotone, rcErodeWalkableArea,
+  rcBuildRegions, rcBuildRegionsMonotone, rcCompactCell, rcCompactSpan,
+  rcErodeWalkableArea,
 };
 
 use crate::{wrappers, Context, Heightfield, Vec3};
@@ -58,6 +59,66 @@ impl<TypeState: CompactHeightfieldState> CompactHeightfield<TypeState> {
 
   pub fn cell_height(&self) -> f32 {
     self.compact_heightfield.ch
+  }
+
+  pub fn cells_iter(&self) -> impl Iterator<Item = Range<usize>> + '_ {
+    // SAFETY: `cells` is guaranteed to have `width` * `heights` cells, and be
+    // well aligned. `cells` is also owned by `self` so the lifetime of the
+    // iterator is tied to `cells`.
+    let raw_cells = unsafe {
+      std::slice::from_raw_parts(
+        self.compact_heightfield.cells,
+        self.grid_width() as usize * self.grid_height() as usize,
+      )
+    };
+
+    raw_cells.iter().map(|cell| {
+      (cell.index() as usize)..(cell.index() + cell.count()) as usize
+    })
+  }
+
+  pub fn cell(&self, index: usize) -> Range<usize> {
+    // SAFETY: `cells` is guaranteed to have `width` * `heights` cells, and be
+    // well aligned.
+    let raw_cells = unsafe {
+      std::slice::from_raw_parts(
+        self.compact_heightfield.cells,
+        self.grid_width() as usize * self.grid_height() as usize,
+      )
+    };
+
+    let cell = &raw_cells[index];
+    (cell.index() as usize)..(cell.index() + cell.count()) as usize
+  }
+
+  pub fn spans_len(&self) -> usize {
+    self.compact_heightfield.spanCount as usize
+  }
+
+  pub fn spans_iter(
+    &self,
+  ) -> impl Iterator<Item = CompactSpan<'_, TypeState>> + '_ {
+    // SAFETY: `spans` is guaranteed to have `spanCount` cells, and be well
+    // aligned.
+    let raw_spans = unsafe {
+      std::slice::from_raw_parts(
+        self.compact_heightfield.spans,
+        self.spans_len(),
+      )
+    };
+
+    raw_spans.iter().map(|span| CompactSpan { compact_heightfield: self, span })
+  }
+
+  pub fn span_areas(&self) -> &[u8] {
+    // SAFETY: `areas` is guaranteed to have `spanCount` elements, and be well
+    // aligned.
+    unsafe {
+      std::slice::from_raw_parts(
+        self.compact_heightfield.areas,
+        self.spans_len(),
+      )
+    }
   }
 }
 
@@ -234,12 +295,155 @@ impl CompactHeightfield<HasRegions> {
   }
 }
 
+pub struct CompactSpan<'compact_heightfield, TypeState>
+where
+  TypeState: CompactHeightfieldState,
+{
+  compact_heightfield: &'compact_heightfield CompactHeightfield<TypeState>,
+  span: &'compact_heightfield rcCompactSpan,
+}
+
+impl<'compact_heightfield, TypeState>
+  CompactSpan<'compact_heightfield, TypeState>
+where
+  TypeState: CompactHeightfieldState,
+{
+  pub fn y_start_u16(&self) -> u16 {
+    self.span.y
+  }
+
+  pub fn y_size_u32(&self) -> u32 {
+    self.span.h()
+  }
+
+  pub fn y_start_f32(&self) -> f32 {
+    self.y_start_u16() as f32 * self.compact_heightfield.cell_height()
+      + self.compact_heightfield.min_bounds().y
+  }
+
+  pub fn y_size_f32(&self) -> f32 {
+    self.y_size_u32() as f32 * self.compact_heightfield.cell_height()
+  }
+
+  pub fn y_end_u32(&self) -> u32 {
+    self.y_start_u16() as u32 + self.y_size_u32()
+  }
+
+  pub fn y_end_f32(&self) -> f32 {
+    self.y_start_f32() + self.y_size_f32()
+  }
+
+  pub fn connection(&self, direction: Direction) -> u32 {
+    let shift = match direction {
+      Direction::NegX => 0,
+      Direction::PosY => 1,
+      Direction::PosX => 2,
+      Direction::NegY => 3,
+    } * 6;
+
+    self.span.con() >> shift & 0x3f
+  }
+}
+
+impl<'compact_heightfield> CompactSpan<'compact_heightfield, HasRegions> {
+  pub fn region_id(&self) -> u16 {
+    self.span.reg
+  }
+}
+
+impl<'compact_heightfield> std::fmt::Debug
+  for CompactSpan<'compact_heightfield, NoRegions>
+{
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct("CompactSpan")
+      .field("y_start", &self.y_start_u16())
+      .field("y_size", &self.y_size_u32())
+      .field(
+        "connections",
+        &[
+          (Direction::NegX, self.connection(Direction::NegX)),
+          (Direction::PosY, self.connection(Direction::PosY)),
+          (Direction::PosX, self.connection(Direction::PosX)),
+          (Direction::NegY, self.connection(Direction::NegY)),
+        ],
+      )
+      .finish()
+  }
+}
+
+impl<'compact_heightfield> std::fmt::Debug
+  for CompactSpan<'compact_heightfield, HasRegions>
+{
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct("CompactSpan")
+      .field("y_start", &self.y_start_u16())
+      .field("y_size", &self.y_size_u32())
+      .field(
+        "connections",
+        &[
+          (Direction::NegX, self.connection(Direction::NegX)),
+          (Direction::PosY, self.connection(Direction::PosY)),
+          (Direction::PosX, self.connection(Direction::PosX)),
+          (Direction::NegY, self.connection(Direction::NegY)),
+        ],
+      )
+      .field("region_id", &self.region_id())
+      .finish()
+  }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Direction {
+  NegX,
+  PosY,
+  PosX,
+  NegY,
+}
+
 #[cfg(test)]
 mod tests {
   use crate::{
     CompactHeightfield, Context, HasRegions, Heightfield, NoRegions, Vec3,
     WALKABLE_AREA_ID,
   };
+
+  macro_rules! assert_span_column_eq {
+    ($column: expr, $expected_column: expr) => {{
+      let column = $column;
+      let expected_column = $expected_column;
+
+      assert_eq!(
+        column.len(),
+        expected_column.len(),
+        "\n\nactual_spans={:?}\nexpected_spans={:?}",
+        column,
+        expected_column
+      );
+
+      for (index, (actual_span, expected_span)) in
+        column.iter().zip(expected_column.iter()).enumerate()
+      {
+        assert_eq!(
+          (
+            actual_span.y_start_u16(),
+            actual_span.y_size_u32(),
+            actual_span.y_end_u32(),
+          ),
+          *expected_span,
+          "\n\nColumn differs at index {}\n\n\
+          actual_span={:?}\n\
+          expected_span=CompactSpan {{ \
+            y_start_u16: {}, \
+            y_size_u16: {}, \
+           }}",
+          index,
+          actual_span,
+          expected_span.0,
+          expected_span.1,
+        );
+      }
+    }};
+  }
 
   #[test]
   fn create_compact_heightfield() {
@@ -287,6 +491,17 @@ mod tests {
       // walked on.
       Vec3::new(max_bounds.x, max_bounds.y + 3.0, max_bounds.z)
     );
+
+    // CompactSpans only store the "free space" rather than the blocked space
+    // like in HeightFieldSpans. The top-most span is always expanded to 256.
+    let expected_column = [(1, 255, 256)];
+
+    let spans = compact_heightfield.spans_iter().collect::<Vec<_>>();
+    for column in
+      compact_heightfield.cells_iter().map(|cell_range| &spans[cell_range])
+    {
+      assert_span_column_eq!(column, &expected_column);
+    }
   }
 
   #[test]
@@ -324,9 +539,32 @@ mod tests {
       )
       .expect("creating CompactHeightfield succeeds");
 
+    const W: u8 = WALKABLE_AREA_ID;
+    assert_eq!(
+      compact_heightfield.span_areas(),
+      [
+        W, W, W, W, W, //
+        W, W, W, W, W, //
+        W, W, W, W, W, //
+        W, W, W, W, W, //
+        W, W, W, W, W, //
+      ]
+    );
+
     compact_heightfield
       .erode_walkable_area(&mut context, 1)
       .expect("erosion succeeds");
+
+    assert_eq!(
+      compact_heightfield.span_areas(),
+      [
+        0, 0, 0, 0, 0, //
+        0, W, W, W, 0, //
+        0, W, W, W, 0, //
+        0, W, W, W, 0, //
+        0, 0, 0, 0, 0, //
+      ]
+    );
   }
 
   fn build_regions_base(
