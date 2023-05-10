1e24784045ddcb9857bf742d149896f82e87433a
andriyDev <andriydzikh@gmail.com>
2023-05-10 14:12:06 -0700

    Add comments for PolyMesh, PolyMeshDetail and related structs.

--- src/poly_mesh.rs
+++ src/poly_mesh.rs
@@ -9,11 +9,14 @@ use crate::{
 
 pub use recastnavigation_sys::RC_MESH_NULL_IDX as NULL_INDEX;
 
+// A Recast polygon mesh. This is essentially the completed navigation mesh.
 pub struct PolyMesh {
   poly_mesh: wrappers::RawPolyMesh,
 }
 
 impl PolyMesh {
+  // Creates a PolyMesh from a ContourSet. `max_vertices_per_polygon` determines
+  // the maximum number of vertices each node/polygon can have.
   pub fn new(
     contour_set: &ContourSet,
     context: &mut Context,
@@ -57,11 +60,13 @@ impl PolyMesh {
     self.poly_mesh.nverts as usize
   }
 
+  // Gets a PolyMeshVertex by index.
   pub fn vertex(&self, index: usize) -> PolyMeshVertex {
     assert!(index < self.vertices_len());
     PolyMeshVertex { poly_mesh: self, index }
   }
 
+  // Creates an Iterator of all vertices.
   pub fn vertices_iter(&self) -> impl Iterator<Item = PolyMeshVertex> + '_ {
     (0..self.vertices_len())
       .map(|index| PolyMeshVertex { poly_mesh: self, index })
@@ -71,11 +76,13 @@ impl PolyMesh {
     self.poly_mesh.npolys as usize
   }
 
+  // Gets a PolyMeshPolygon by index.
   pub fn polygon(&self, index: usize) -> PolyMeshPolygon {
     assert!(index < self.polygons_len());
     PolyMeshPolygon { poly_mesh: self, index }
   }
 
+  // Creates an Iterator of all polygons.
   pub fn polygons_iter(&self) -> impl Iterator<Item = PolyMeshPolygon> + '_ {
     (0..self.polygons_len())
       .map(|index| PolyMeshPolygon { poly_mesh: self, index })
@@ -118,16 +125,20 @@ impl PolyMesh {
   }
 }
 
+// A single vertex in a PolyMesh. This is used to select how to represent the
+// vertex (either in grid units or in "world" units).
 pub struct PolyMeshVertex<'poly_mesh> {
   poly_mesh: &'poly_mesh PolyMesh,
   index: usize,
 }
 
 impl<'poly_mesh> PolyMeshVertex<'poly_mesh> {
+  // Gets the vertex as its grid coordinates.
   pub fn as_u16(&self) -> Vec3<u16> {
     self.poly_mesh.raw_vertices()[self.index]
   }
 
+  // Gets the vertex in "world space".
   pub fn as_f32(&self) -> Vec3<f32> {
     let raw_vector = self.as_u16();
     Vec3::<f32>::new(
@@ -138,12 +149,18 @@ impl<'poly_mesh> PolyMeshVertex<'poly_mesh> {
   }
 }
 
+// A single polygon in a PolyMesh.
 pub struct PolyMeshPolygon<'poly_mesh> {
   poly_mesh: &'poly_mesh PolyMesh,
   index: usize,
 }
 
 impl<'poly_mesh> PolyMeshPolygon<'poly_mesh> {
+  // Gets the "raw" indices of each vertex in the polygon. This will always have
+  // a length of `poly_mesh.max_vertices_per_polygon`. Polygons with fewer
+  // vertices than the max will fill the remaining entries with `NULL_INDEX`.
+  // Use `valid_vertices` to get the valid slice or use an iterator to filter
+  // out `NULL_INDEX` values.
   pub fn vertices(&self) -> &'poly_mesh [u16] {
     let nvp = self.poly_mesh.poly_mesh.nvp as usize;
 
@@ -162,6 +179,12 @@ impl<'poly_mesh> PolyMeshPolygon<'poly_mesh> {
     &polys[start_index..(start_index + nvp)]
   }
 
+  // Gets the "raw" indices of the neighbours of each edge in the polygon. This
+  // will always have a length of `poly_mesh.max_vertices_per_polygon`.
+  // Polygons with fewer edges (equivalent to number of vertices) than the max
+  // will fill the remaining entries with `NULL_INDEX`. Use `valid_neighbours`
+  // to get the valid slice or use an iterator to filter out `NULL_INDEX`
+  // values.
   pub fn neighbours(&self) -> &'poly_mesh [u16] {
     let nvp = self.poly_mesh.poly_mesh.nvp as usize;
 
@@ -181,6 +204,10 @@ impl<'poly_mesh> PolyMeshPolygon<'poly_mesh> {
     &polys[start_index..(start_index + nvp)]
   }
 
+  // Same as `vertices`, but sliced to only include indices that aren't
+  // `NULL_INDEX`. Note this involves a linear search to find the end of the
+  // valid vertices, so it may be more expensive than simply filtering out
+  // `NULL_INDEX`.
   pub fn valid_vertices(&self) -> &'poly_mesh [u16] {
     let vertices = self.vertices();
     let index = vertices
@@ -193,6 +220,10 @@ impl<'poly_mesh> PolyMeshPolygon<'poly_mesh> {
     &vertices[..index]
   }
 
+  // Same as `neighbours`, but sliced to only include indices that aren't
+  // `NULL_INDEX`. Note this involves a linear search to find the end of the
+  // valid neighbours, so it may be more expensive than simply filtering out
+  // `NULL_INDEX`.
   pub fn valid_neighbours(&self) -> &'poly_mesh [u16] {
     let neighbours = self.neighbours();
 
@@ -241,11 +272,17 @@ impl<'poly_mesh> PolyMeshPolygon<'poly_mesh> {
   }
 }
 
+// A Recast detailed polygon mesh. This is a triangle mesh that stores detailed
+// height data for each polygon in its associated PolyMesh.
 pub struct PolyMeshDetail {
   poly_mesh_detail: wrappers::RawPolyMeshDetail,
 }
 
 impl PolyMeshDetail {
+  // Creates a PolyMeshDetail from a PolyMesh and a CompactHeightfield.
+  // `sample_distance` is how frequently (in world units) to samples the height
+  // at. `sample_max_error` is the max distance that the mesh surface should
+  // deviate from the heightfield data.
   pub fn new(
     poly_mesh: &PolyMesh,
     context: &mut Context,
@@ -277,6 +314,7 @@ impl PolyMeshDetail {
     }
   }
 
+  // Vertices for all polygons in the detail mesh.
   pub fn vertices(&self) -> &[Vec3<f32>] {
     // SAFETY: `verts` has `nverts` * 3 f32's, so casting to `nverts`
     // Vec3<f32>'s is safe. The slice is well aligned and non-null since
@@ -293,11 +331,13 @@ impl PolyMeshDetail {
     self.poly_mesh_detail.nmeshes as usize
   }
 
+  // Gets a single PolyMeshDetailSubmesh by its index.
   pub fn submesh(&self, index: usize) -> PolyMeshDetailSubmesh<'_> {
     assert!(index <= self.submeshes_len());
     PolyMeshDetailSubmesh { poly_mesh_detail: self, index }
   }
 
+  // Creates an Iterator of all submeshes.
   pub fn submeshes_iter(
     &self,
   ) -> impl Iterator<Item = PolyMeshDetailSubmesh> + '_ {
@@ -306,6 +346,8 @@ impl PolyMeshDetail {
   }
 }
 
+// A single submesh of a PolyMeshDetail. A submesh generally represents a single
+// node/polygon of a PolyMesh.
 pub struct PolyMeshDetailSubmesh<'poly_mesh_detail> {
   poly_mesh_detail: &'poly_mesh_detail PolyMeshDetail,
   index: usize,
@@ -325,6 +367,7 @@ impl<'poly_mesh_detail> PolyMeshDetailSubmesh<'poly_mesh_detail> {
     &raw_meshes[(self.index * 4)..(self.index * 4 + 4)]
   }
 
+  // Gets a slice of the vertices that make up this submesh.
   pub fn vertices(&self) -> &'poly_mesh_detail [Vec3<f32>] {
     let raw_mesh_data = self.raw_mesh_data();
     let vert_start = raw_mesh_data[0] as usize;
@@ -337,6 +380,7 @@ impl<'poly_mesh_detail> PolyMeshDetailSubmesh<'poly_mesh_detail> {
     raw_mesh_data[3] as usize
   }
 
+  // Creates an Iterator of triangles.
   pub fn triangles_iter(
     &self,
   ) -> impl Iterator<Item = PolyMeshDetailTriangle<'poly_mesh_detail>>
@@ -359,6 +403,7 @@ impl<'poly_mesh_detail> PolyMeshDetailSubmesh<'poly_mesh_detail> {
     })
   }
 
+  // Gets a single PolyMeshDetailTriangle by its index.
   pub fn triangle(
     &self,
     index: usize,
@@ -382,11 +427,13 @@ impl<'poly_mesh_detail> PolyMeshDetailSubmesh<'poly_mesh_detail> {
   }
 }
 
+// A single triangle in a PolyMeshDetail.
 pub struct PolyMeshDetailTriangle<'poly_mesh_detail> {
   raw_triangle_data: &'poly_mesh_detail [u8],
 }
 
 impl<'poly_mesh_detail> PolyMeshDetailTriangle<'poly_mesh_detail> {
+  // Gets the indices of the vertices that make up the triangle as a triplet.
   pub fn vertex_indices(&self) -> (usize, usize, usize) {
     (
       self.raw_triangle_data[0] as usize,
@@ -395,6 +442,9 @@ impl<'poly_mesh_detail> PolyMeshDetailTriangle<'poly_mesh_detail> {
     )
   }
 
+  // Returns whether each edge of the triangle is on the boundary of the
+  // PolyMesh. The boundaries are recorded as (index_0_to_index_1,
+  // index_1_to_index_2, index_2_to_index_0).
   pub fn are_edges_on_mesh_boundary(&self) -> (bool, bool, bool) {
     let triangle_flags = self.raw_triangle_data[3];
     (
